5bcaffd81dca869d82e162764e7e12a450b5f4d7
Arunesh Agrawal <arunesh.agrawal1998@gmail.com>
2023-09-29 12:06:14 +0530

    updated commit message

--- 05.Array_or_List_Practice_problems/6.2D_matrix_transpotation_and_rotation.py
+++ 05.Array_or_List_Practice_problems/6.2D_matrix_transpotation_and_rotation.py
@@ -4,7 +4,7 @@
 # Output: [[7,4,1],[8,5,2],[9,6,3]]
 
 
-def rotate(matrix):
+def rotate_90_clockwise(matrix):
 
     # Transpose the matrix
     for i in range(len(matrix)):
@@ -19,4 +19,4 @@ def rotate(matrix):
 
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(rotate(matrix))
+print(rotate_90_clockwise(matrix))
