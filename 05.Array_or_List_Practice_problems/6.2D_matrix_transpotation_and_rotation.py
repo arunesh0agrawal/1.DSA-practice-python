ecebc5adbd1a61654d77e43ca9aca1e6838dab46
Arunesh Agrawal <arunesh.agrawal1998@gmail.com>
2023-09-29 12:20:43 +0530

    added more way to rotate matrix

--- 05.Array_or_List_Practice_problems/6.2D_matrix_transpotation_and_rotation.py
+++ 05.Array_or_List_Practice_problems/6.2D_matrix_transpotation_and_rotation.py
@@ -3,13 +3,15 @@
 # Example : matrix = [[1,2,3],[4,5,6],[7,8,9]]
 # Output: [[7,4,1],[8,5,2],[9,6,3]]
 
+# method 1: (little pythonic)
 
-def rotate_90_clockwise(matrix):
+
+def rotate_90_clockwise(matrix):  # [ T(n) : O(n2), S(n): O(1)]
 
     # Transpose the matrix
     for i in range(len(matrix)):
         for j in range(i, len(matrix)):
-            # swapping elements (i, j) and (j, i)
+            # Swapping elements (i, j) and (j, i)
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     for row in matrix:
@@ -18,5 +20,20 @@ def rotate_90_clockwise(matrix):
     return matrix
 
 
+# method 2: (Algorithmic way)
+
+
+def rotate_90_clockwise_algo(matrix):
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
+    return matrix
+
+
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(rotate_90_clockwise(matrix))
+print(rotate_90_clockwise_algo(matrix))
