00b974bab5d7c0917ecef1be45bc458471054b0a
Arunesh <arunesh.agrawal1998@gmail.com>
2024-01-08 21:30:18 +0530

    adding topic wise question folder

---
+++ 27.topic_wise_practice_problems/1.Array_and_Hashmap/1.contains_duplicate.py
@@ -0,0 +1,37 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+def containsDuplicate(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+
+    return False
+
+# Example usage:
+nums1 = [1, 2, 3, 1]
+print(containsDuplicate(nums1))  # Output: True
+
+nums2 = [1, 2, 3, 4]
+print(containsDuplicate(nums2))  # Output: False
+
+nums3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+print(containsDuplicate(nums3))  # Output: True
+
+
+# The time complexity of this solution is O(n), where n is the length of the input array nums
